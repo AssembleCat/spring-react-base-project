6eda780fa520dcd1f30df7284ecfd239b1ebd355
syCheon <cheonshin99@hs.ac.kr>
2023-05-23 17:18:55 +0900

    [docs] FlywayMigration 규칙을 따른 SQL문 예시 설명추가

--- migration/V001__MIGRATION_EXAMPLE.sql
+++ migration/V001__MIGRATION_EXAMPLE.sql
@@ -1,3 +1,8 @@
+-- Vnnn__MIGRAION_NAME.sql
+-- SQL 파일명에 다음 규칙을 따르면 Migration시 자동적용됩니다.
+-- 1. 설정파일(application.yml) 'spring.flyway.locations' 에 파일이 위치할것
+-- 2. 'V'문자로 시작 후 실행순서에 따른 숫자를 적습니다. ex) V001, V022, V52
+
 CREATE TABLE BOOK
 (
     TITLE     VARCHAR(100) NOT NULL,
