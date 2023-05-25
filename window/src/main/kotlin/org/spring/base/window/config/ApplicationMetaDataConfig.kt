e86eced63a139de560b1a730a49ccfa79319facc
syCheon <cheonshin99@hs.ac.kr>
2023-05-25 15:06:56 +0900

    [fix] MetaData load 방식 변경
    project.yaml, version.yaml 처럼 Static한 정보를 Load할 경우가
    많아질듯하여 metadata폴더에서 경로로 Bean등록할 수 있도록 수정

--- window/src/main/kotlin/org/spring/base/window/config/ApplicationMetaDataConfig.kt
+++ window/src/main/kotlin/org/spring/base/window/config/ApplicationMetaDataConfig.kt
@@ -5,13 +5,18 @@ import org.spring.base.window.config.data.ProjectInformation
 import org.spring.base.window.config.data.VersionInformation
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.yaml.snakeyaml.Yaml
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
- * Application운영에 필요한 추가 정보들을 Bean 등록
+ * Application운영에 필요한 MetaData Bean 등록
  * ```
  * - VersionInformation(버전정보)
  * - ProjectInformation(프로젝트정보)
+ * - NotificationInformation(알림정보)
  * ```
  * @see VersionInformation
  * @see ProjectInformation
@@ -24,13 +29,12 @@ class ApplicationMetadataConfig(
     fun versionInformation(): VersionInformation {
         // Import version.yaml inside resources classpath
         val version = loadResource("version.yaml")
-        // version.yaml is multiple documents in single file
-        // so we need to parse it as list
+        // version.yaml have multiple documents in single file
+        // So we need to parse it as a list
         val currentProfile = System.getProperty("spring.profiles.active")
         return Yaml().loadAll(version).map { objectMapper.convertValue(it, VersionInformation::class.java) }
             .firstOrNull { it.phase == currentProfile }
             ?: throw Exception("version.yaml for this profile not found: $currentProfile")
-
     }
 
     @Bean
@@ -40,8 +44,15 @@ class ApplicationMetadataConfig(
             .let { return objectMapper.convertValue(it, ProjectInformation::class.java) }
     }
 
-    private fun loadResource(name: String) =
-        javaClass.classLoader.getResource(name)?.readText()
-            ?: throw Exception("Resource $name not found")
+    private fun loadResource(filePath: String): String {
+        val resource = ClassPathResource("metadata/$filePath")
+        val file = resource.file
+
+        return try {
+            Files.readAllLines(Paths.get(file.toURI()), StandardCharsets.UTF_8).joinToString("\n")
+        } catch (e: Exception) {
+            throw RuntimeException("Failed to read resource: $filePath", e)
+        }
+    }
 
 }
