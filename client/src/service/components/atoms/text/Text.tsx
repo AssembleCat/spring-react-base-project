b3e9eaac7145bee707c4b0747de9ee701ef12e65
syCheon <cheonshin99@hs.ac.kr>
2023-05-25 16:10:29 +0900

    [feature] Text Atom Component 생성
    font-size, text-class-name, container-class-name(각 텍스트 줄을 모두
    포함한 div), click-event, ('\n') 줄바꿈 분할 기능

---
+++ client/src/service/components/atoms/text/Text.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface TextProps {
+    containerClassName?: string;
+    textClassName?: string;
+    textSize?: number;
+    text?: string | number;
+    clickEvent?: React.MouseEventHandler;
+}
+
+const Text: React.FC<TextProps> = ({
+                                       containerClassName,
+                                       textClassName,
+                                       textSize,
+                                       text,
+                                       clickEvent,
+                                   }) => {
+    const defaultText = (text !== null && text !== undefined) ? String(text) : "";
+    const splitedText = defaultText.split("\n");
+
+    return (
+        <div className={containerClassName}>
+            {splitedText.map((text, index) => (
+                <p
+                    key={index}
+                    className={textClassName}
+                    onClick={clickEvent}
+                    style={textSize ? {fontSize: textSize} : undefined}
+                >
+                    {text}
+                </p>
+            ))}
+        </div>
+    );
+};
+
+export default React.memo(Text);
